740007025b5bb8c0866202ec7bcba7f555f28543
JoJoJoin <137604260@qq.com>
2026-06-29 18:32:16 +0800

    fix-riverpod3-stateprovider-import

--- lib/data/providers.dart
+++ lib/data/providers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 import 'db/database.dart';
 import 'repositories/cooking_log_repository.dart';
